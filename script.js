eddfa2439306e6d87090711bb22d9e5ae7b4f92a
Auxillen <auxillen18@gmail.com>
2025-05-18 08:45:02 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,77 +1,20 @@
-@import url('https://fonts.googleapis.com/css?family=Montserrat:700');
+document.addEventListener("DOMContentLoaded", () => {
+  const titleText = document.querySelector(".title h1");
+  const roleText = document.querySelector(".role p");
 
-body {
-  margin: 0;
-  padding: 0;
-  background: #000;
-  font-family: 'Montserrat', sans-serif;
-  overflow: hidden;
-}
+  // Initially hide the text
+  titleText.style.opacity = "0";
+  roleText.style.opacity = "0";
 
-.container {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 100vh;
-}
+  // Reveal the title after the first animation (1s)
+  setTimeout(() => {
+    titleText.style.transition = "opacity 0.5s ease-in-out";
+    titleText.style.opacity = "1";
+  }, 1000);
 
-.box {
-  text-align: center;
-}
-
-.title {
-  position: relative;
-  color: #fff;
-  font-size: 4em;
-}
-
-.title h1 {
-  position: relative;
-  margin: 0;
-  padding: 0;
-  z-index: 2;
-}
-
-.title span.block {
-  position: absolute;
-  width: 0%;
-  height: 100%;
-  background: #f00;
-  left: 0;
-  top: 0;
-  animation: slide 1s ease forwards;
-  z-index: 1;
-}
-
-.role {
-  position: relative;
-  color: #fff;
-  font-size: 2em;
-  margin-top: 20px;
-}
-
-.role .block {
-  position: absolute;
-  width: 0%;
-  height: 100%;
-  background: #00f;
-  left: 0;
-  top: 0;
-  animation: slide 1s ease 1s forwards;
-  z-index: 1;
-}
-
-@keyframes slide {
-  0% {
-    width: 0%;
-    left: 0;
-  }
-  50% {
-    width: 100%;
-    left: 0;
-  }
-  100% {
-    width: 0%;
-    left: 100%;
-  }
-}
+  // Reveal the role text after the second block animation (2s)
+  setTimeout(() => {
+    roleText.style.transition = "opacity 0.5s ease-in-out";
+    roleText.style.opacity = "1";
+  }, 2000);
+});
